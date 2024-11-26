932e80ff3ecfb6954da415059c80e45fd4201563
ArturoAceves1999 <92830028+ArturoAceves1999@users.noreply.github.com>
2024-11-26 09:42:14 -0600

    New hdvp and py version
    Added the filter that do the cleaning of the image to obtain the regions of interest and dilation of those.

--- Python - HALCON Tests/pruebahalconpython.py
+++ Python - HALCON Tests/pruebahalconpython.py
@@ -3,6 +3,8 @@ import os
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+import time
+import math
 
 
 def variablesStart():
@@ -43,20 +45,6 @@ def cameraInitialize(xresolution, yresolution, framescamera):
         pipeline = rs.pipeline()
         config = rs.config()
 
-        pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-        pipeline_profile = config.resolve(pipeline_wrapper)
-        device = pipeline_profile.get_device()
-        device_product_line = str(device.get_info(rs.camera_info.product_line))
-
-        found_rgb = False
-        for s in device.sensors:
-            if s.get_info(rs.camera_info.name) == 'RGB Camera':
-                found_rgb = True
-                break
-        if not found_rgb:
-            print("The demo requires Depth camera with Color sensor")
-            exit(0)
-
         config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
         config.enable_stream(rs.stream.color, xresolution, yresolution, rs.format.bgr8, framescamera)
         # Start streaming
@@ -71,8 +59,8 @@ def cameraInitialize(xresolution, yresolution, framescamera):
 def cameraData(pipeline, externalCall, Multi):
     try:
         while True:
-
             # Wait for a coherent pair of frames: depth and color
+            start = time.time()
             frames = pipeline.wait_for_frames()
             depth_frame = frames.get_depth_frame()
             color_frame = frames.get_color_frame()
@@ -103,13 +91,17 @@ def cameraData(pipeline, externalCall, Multi):
             #print(deptVal)
 
             # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+            depth_colormap = cv2.rotate(cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET), cv2.ROTATE_180)
             depthHALCON_colormap = cv2.applyColorMap(imageReturnHalcon2, cv2.COLORMAP_JET)
 
             cv2.circle(depthHALCON_colormap, (midx, midy), 4, (0, 0, 255), -1)
             cv2.putText(depthHALCON_colormap, "{} mm".format(deptVal), (midx, midy - 10), 0, 1, (0, 0, 255), 2)
             cv2.circle(depth_colormap, (midx, midy), 4, (0, 0, 255), -1)
             cv2.putText(depth_colormap, "{} mm".format(depthpoint), (midx, midy - 10), 0, 1, (0, 0, 255), 2)
+            #time.sleep(0.05)
+
+            end = time.time()
+            print("FPS: ",round(1/(end-start)))
 
             depth_colormap_dim = depth_colormap.shape
             color_colormap_dim = color_image.shape
