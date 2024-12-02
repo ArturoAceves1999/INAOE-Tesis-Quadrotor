30eaf17496f450b06b3d830b3907d9c7db3189bf
ArturoAceves1999 <92830028+ArturoAceves1999@users.noreply.github.com>
2024-12-02 15:39:14 -0600

    Update pruebahalconpython.py

--- Python - HALCON Tests/pruebahalconpython.py
+++ Python - HALCON Tests/pruebahalconpython.py
@@ -131,6 +131,7 @@ def main():
     midx = int(xresolution/2)
     midy = int(yresolution/2)
     oldXYfarthest = [0, 0]
+    oldimagehalcon =np.zeros((xresolution,yresolution))
     if externalcall == 0:
         exit(0)
     pipeline = cameraInitialize(xresolution, yresolution, framescamera)
