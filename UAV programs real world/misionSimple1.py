140a9596bf8363eadcab9ddbd4043977585b11f9
Arturo Aceves <maclon31@hotmail.com>
2025-07-06 11:30:07 -0600

    Added UAV programs for real world implementation

---
+++ UAV programs real world/misionSimple1.py
@@ -0,0 +1,267 @@
+from __future__ import print_function
+
+from dronekit import connect, VehicleMode, LocationGlobalRelative, LocationGlob>
+from pymavlink import mavutil
+import time
+import socket
+import math
+import argparse
+import datetime
+import serial
+
+
+def seriaInitSerial():
+    serial_port = serial.Serial(
+        port="/dev/ttyAMA5",
+        baudrate=115200,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+    )
+    # Wait a second to let the port initialize
+    time.sleep(1)
+    
+    mensaje = "SerialStart\n"  # ejemplo: x y z separados por espacio
+    serial_port.write(mensaje.encode('utf-8'))
+    
+    return serial_port
+
+def connectMyCopter():
+    parser = argparse.ArgumentParser(description='Misión simple con DroneKit y Comunicacion serial.')
+    parser.add_argument('--connect', help="Cadena de conexión al vehículo.")
+    args = parser.parse_args()
+    connection_string = args.connect
+    baud_rate = 57600
+
+    vehicle = connect(connection_string,baud=baud_rate,wait_ready=True)
+    return vehicle
+
+
+def get_location_metres(original_location, dNorth, dEast):
+    earth_radius = 6378137.0
+    dLat = dNorth / earth_radius
+    dLon = dEast / (earth_radius * math.cos(math.pi * original_location.lat / 180))
+    newlat = original_location.lat + (dLat * 180 / math.pi)
+    newlon = original_location.lon + (dLon * 180 / math.pi)
+    return LocationGlobal(newlat, newlon, original_location.alt)
+
+def get_location_metres_simple(archi1, location, takeofflocation, isevading):
+    earth_radius = 6378137.0
+    
+    dlat = math.radians(location.lat - takeofflocation.lat)
+    dlon = math.radians(location.lon - takeofflocation.lon)
+    x = dlon * earth_radius * math.cos(math.radians(takeofflocation.lat))
+    y = dlat * earth_radius
+    
+    archi1.write(str(x) + " " + str(y) + " " + str(location.alt) + " " + str(isevading)  + "\n") 
+
+def get_distance_metres(aLocation1, aLocation2):
+    dlat = aLocation2.lat - aLocation1.lat
+    dlong = aLocation2.lon - aLocation1.lon
+    return math.sqrt((dlat * dlat) + (dlong * dlong)) * 1.113195e5
+
+
+def distance_to_current_waypoint(vehicle):
+    nextwaypoint = vehicle.commands.next
+    if nextwaypoint == 0:
+        return None
+    missionitem = vehicle.commands[nextwaypoint - 1]
+    lat = missionitem.x
+    lon = missionitem.y
+    alt = missionitem.z
+    targetWaypointLocation = LocationGlobalRelative(lat, lon, alt)
+    return get_distance_metres(vehicle.location.global_frame, targetWaypointLocation)
+
+
+def adds_square_mission(vehicle, aLocation, aSize):
+    cmds = vehicle.commands
+    print(" Clear any existing commands")
+    cmds.clear()
+
+    print(" Define/add new commands.")
+    cmds.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+                     mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 0, 10))
+
+    point1 = get_location_metres(aLocation, aSize, 0)
+    point2 = get_location_metres(aLocation, aSize+aSize, aSize)
+    #point3 = get_location_metres(aLocation, -aSize, aSize)
+    #point4 = get_location_metres(aLocation, -aSize, -aSize)
+    point4 = get_location_metres(aLocation, 0, 0)
+    for idx, point in enumerate([point1, point2, point4]):
+        cmds.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+                         mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0,
+                         point.lat, point.lon, 2))
+    # Dummy WP 5
+    cmds.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+                     mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0,
+                     point4.lat, point4.lon, 2))
+
+    print(" Upload new commands to vehicle")
+    cmds.upload()
+
+
+def arm_and_takeoff(vehicle, aTargetAltitude):
+    print("Basic pre-arm checks")
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude)
+
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.8:
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+        
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0, 0, 0,
+        mavutil.mavlink.MAV_FRAME_LOCAL_NED,
+        2503,
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0,
+        0, 0
+    )
+    
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
+    vehicle.airspeed = 0.5
+    
+    time.sleep(5)
+    
+
+def get_distance_local_ned(current, target):
+    dx = target[0] - current.north
+    dy = target[1] - current.east
+    dz = target[2] - current.down
+    return math.sqrt(dx**2 + dy**2 + dz**2)
+
+def goto_evation(vehicle, directions, archi1, originallocation):
+    print("Entering EVATION mode")
+    vehicle.mode = VehicleMode("GUIDED")
+    
+    forward = directions[2]
+    right = directions[0]
+    down = -directions[1]
+    
+    yaw_deg = vehicle.heading
+    yaw_rad = math.radians(yaw_deg)
+    north = math.cos(yaw_rad) * forward - math.sin(yaw_rad) * right
+    east  = math.sin(yaw_rad) * forward + math.cos(yaw_rad) * right
+    loc = vehicle.location.local_frame
+    target = (loc.north + north, loc.east + east, loc.down + down)
+    
+    print(f"Evasión hacia (N, E, D): {target}")
+    
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0, 0, 0,
+        mavutil.mavlink.MAV_FRAME_LOCAL_NED,
+        1528,
+        target[0], target[1], target[2], 
+        0, 0, 0,
+        0, 0, 0,
+        0, 0
+    )
+    
+    for _ in range(20):  # máx 5 segundos
+        vehicle.send_mavlink(msg)
+        vehicle.flush()
+        vehicle.airspeed = 0.5
+        loc = vehicle.location.local_frame
+        dist = math.sqrt((target[0] - loc.north)**2 + (target[1] - loc.east)**2 + (target[2] - loc.down)**2)
+        get_location_metres_simple(archi1, vehicle.location.global_relative_frame, originallocation,1)
+        print(f"Distancia restante: {dist:.2f} m")
+
+        if dist < 0.1:
+            print("Evasión completada.")
+            break
+            
+        time.sleep(0.1)
+    
+    vehicle.mode = VehicleMode("AUTO")
+    vehicle.airspeed = 0.5
+
+
+def main():
+    serialcom = seriaInitSerial()
+    date = datetime.datetime.now()
+    archi1=open("LOG " + date.strftime('%c'),"w") 
+    
+    print("Beggining program, please wait")
+    vehicle = connectMyCopter()
+
+
+    print('Creando misión')
+    #adds_square_mission(vehicle, vehicle.location.global_frame, 5)
+    #arm_and_takeoff(vehicle, 2)
+    originallocation = vehicle.location.global_relative_frame 
+    get_location_metres_simple(archi1, originallocation,originallocation, 0)
+    
+    print("Iniciando misión")
+    #vehicle.commands.next = 0
+    #vehicle.mode = VehicleMode("AUTO")
+    #vehicle.airspeed = 0.5
+
+    try:
+        while True:
+            get_location_metres_simple(archi1, vehicle.location.global_relative_frame, originallocation, 0)
+            latest_msg = None
+
+            while serialcom.in_waiting:
+                try:
+                # Leer todo lo que haya en el buffer
+                    raw = serialcom.readline().decode("utf-8").strip()
+                    latest_msg = raw  # Solo nos quedamos con el último válido
+                except UnicodeDecodeError:
+                    continue  # Saltar si se corrompe un paquete
+
+            if latest_msg:
+                print(f"Último mensaje útil: {latest_msg}")
+                try:
+                    data = [float(p) for p in latest_msg.split()]
+                    evationmode = int(data[0])
+                    directions = data[1:]
+                
+                    if evationmode == 1:
+                        print("¡Comando de evasión recibido!")
+                        #goto_evation(vehicle, directions,archi1, originallocation)
+                    elif evationmode == 2:
+                        print("¡Comando de evasión EMERGENCIA recibido!")
+                        #goto_evation(vehicle, [0,0,-1.5], archi1, originallocation)
+                    elif evationmode == 0:
+                        print("No es necesario evadir")
+                
+                except ValueError:
+                    print("Mensaje mal formado: ", latest_msg)
+
+            #nextwaypoint = vehicle.commands.next
+            #vehicle.airspeed = 0.5
+            print(f'Distance to waypoint ({nextwaypoint}): {distance_to_current_waypoint(vehicle)}')
+
+            #if nextwaypoint == 3:
+                #print("Finalizando misión")
+                #break
+
+            time.sleep(1)
+
+    finally:
+        print('Return to launch')
+        #vehicle.mode = VehicleMode("LAND")
+        print("Closing vehicle")
+        vehicle.close()
+        if serialcom and serialcom.is_open:
+            serialcom.close()
+
+        archi1.close() 
+
---
+++ UAV programs real world/pruebahalconpython.py
@@ -0,0 +1,257 @@
+import halcon as ha
+import os
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+import time
+import math
+import rclpy
+#from types import SimpleNamespace
+import serial
+
+def seriaInitSerial():
+    serial_port = serial.Serial(
+        port="/dev/ttyTHS1",
+        baudrate=115200,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+    )
+    # Wait a second to let the port initialize
+    time.sleep(1)
+    
+    mensaje = "SerialStart\n"  # ejemplo: x y z separados por espacio
+    serial_port.write(mensaje.encode('utf-8'))
+    
+    return serial_port
+
+
+
+
+def initvariables():
+    class initvar:
+        xresolution = 640
+        yresolution = 480
+        FOVXcamera = 87
+        framescamera = 30
+        numdivisions = 20
+        diffXYpoint = 20
+
+        # Scale of image values
+        minrange = 0
+        maxrange = 7000
+        multi = 255.0 / (maxrange - minrange)
+        tresholdmax = 2500
+        tresholdmin = 550
+        tresholdstop = 500
+        areachange = 4000 #Range between 0-307,200 if 640X480. If bigger, the image will need an even bigger change between frames to be modified
+        percentevasion = 0.8
+        areaevasion = int(percentevasion*(xresolution*yresolution)) #Range between 0-307,200 if 640X480
+        # Drone dimension
+        frameradius = 330 #On mm
+
+        frameratio = (xresolution*frameradius)/(2*math.tan(math.radians(FOVXcamera/2)))
+        sensibility = 1.1
+        secdistanceincrease = 0.9 #Used for increasing distance values on X and Y for evasion. If bigger, the incresing on short distances is going to be bigger, but smaller on longer distances
+
+
+    return initvar
+
+
+def halconInitialize():
+    print("Initializing Halcon programs, please wait...")
+    externalcall = 0
+    try:
+        hdev_engine = ha.HDevEngine()
+        hdev_engine.set_attribute("debug_port", 57786)
+        hdev_engine.set_attribute("debug_password", "1234")
+        hdev_engine.start_debug_server()
+        hdev_engine.set_procedure_path(os.getcwd())
+        external = ha.HDevProcedure.load_external('processImageHalcon')
+        externalcall = ha.HDevProcedureCall(external)
+        print("Halcon programs initialized successfully!")
+    except Exception as e:
+        externalcall = 0
+        print(f"Halcon programs couldn't be initialized, please check: {e}")
+    finally:
+        return externalcall
+
+
+def cameraInitialize(xresolution, yresolution, framescamera):
+    print("Initializing camera, please wait...")
+    pipeline = 0
+    try:
+        pipeline = rs.pipeline()
+        config = rs.config()
+
+        config.enable_stream(rs.stream.depth, xresolution, yresolution, rs.format.z16, framescamera)
+        config.enable_stream(rs.stream.color, xresolution, yresolution, rs.format.bgr8, framescamera)
+        # Start streaming
+        pipeline.start(config)
+        print("Camera initialized successfully!")
+    except Exception as e:
+        pipeline = 0
+        print(f"Camera couldn't be initialized, please check: {e}")
+    finally:
+        return pipeline
+
+
+def cameraData(pipeline):
+    # Wait for a coherent pair of frames: depth and color
+    frames = pipeline.wait_for_frames()
+    depth_frame = frames.get_depth_frame()
+    color_frame = frames.get_color_frame()
+    hole_filling = rs.hole_filling_filter(2)
+    filled_depth = hole_filling.process(depth_frame)
+
+    """
+    if not depth_frame or not color_frame:
+        continue
+    """
+
+    # Convert images to numpy arrays
+    depth_image = np.asanyarray(filled_depth.get_data())
+    depth_image = depth_image * np.exp(depth_image * -0.00001)
+    depth_image = depth_image.astype(np.uint16)
+    color_image = np.asanyarray(color_frame.get_data())
+    return depth_image, color_image
+
+
+def halconProcessing(depthimage, externalcall, initialVariables, oldXYfarthest, oldimagehalcon):
+    # now = datetime.datetime.now()
+    depthimageHalcon = ha.himage_from_numpy_array(depthimage)
+    oldimagehalcon = ha.himage_from_numpy_array(oldimagehalcon)
+    # print(type(depth_imageHalcon))
+
+    externalcall.set_input_iconic_param_by_name('Z', depthimageHalcon)
+    externalcall.set_input_iconic_param_by_name('OldFrame', oldimagehalcon)
+    externalcall.set_input_control_param_by_name('Multi', initialVariables.multi)
+    externalcall.set_input_control_param_by_name('TresholdDistance', initialVariables.tresholdmax)
+    externalcall.set_input_control_param_by_name('AreaChange', initialVariables.areachange)
+    externalcall.set_input_control_param_by_name('AreaEvasion', initialVariables.areaevasion)
+    externalcall.set_input_control_param_by_name('OldXYFarthestPoint', oldXYfarthest)
+    externalcall.set_input_control_param_by_name('UAVFrameRatio', initialVariables.frameratio)
+    externalcall.set_input_control_param_by_name('TresholdDistanceMin', initialVariables.tresholdmin)
+    externalcall.set_input_control_param_by_name('NumIntervals', initialVariables.numdivisions)
+    externalcall.set_input_control_param_by_name('DilationSensibility', initialVariables.sensibility)
+    externalcall.set_input_control_param_by_name('TresholdDistanceStop', initialVariables.tresholdstop)
+    externalcall.set_input_control_param_by_name('DifferenceXYPoint', initialVariables.diffXYpoint)
+    externalcall.set_input_control_param_by_name('SecurityDistanceIncrease', initialVariables.secdistanceincrease)
+    
+    externalcall.execute()
+    imagereturnhalcon = externalcall.get_output_iconic_param_by_name('ImageHalcon')
+    deptval = externalcall.get_output_control_param_by_name('DeptVal')
+    XYfarthest = externalcall.get_output_control_param_by_name('XYFarthestPoint')
+    test = externalcall.get_output_control_param_by_name('test')
+    evasionmode = externalcall.get_output_control_param_by_name('EvasionMode')
+    newdistance = externalcall.get_output_control_param_by_name('NewDistance')
+    imagereturnhalcon2 = ha.himage_as_numpy_array(imagereturnhalcon)
+    # print(imageReturnHalcon2.shape)
+    # print(deptVal)
+
+    return imagereturnhalcon2, XYfarthest, deptval, evasionmode, newdistance
+
+
+def showImages(depth_image, imageReturnHalcon2, color_image, deptVal, XYfarthest, evasionmode, midx, midy):
+    # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+    depth_colormap = cv2.rotate(cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET),
+                                cv2.ROTATE_180)
+    color_image = cv2.rotate(color_image, cv2.ROTATE_180)
+    depthHALCON_colormap = cv2.applyColorMap(imageReturnHalcon2, cv2.COLORMAP_JET)
+
+    cv2.circle(depthHALCON_colormap, (midx, midy), 4, (0, 0, 255), -1)
+    cv2.putText(depthHALCON_colormap, "{} mm".format(deptVal), (midx, midy - 10), 0, 1, (0, 0, 255), 2)
+    # cv2.circle(depth_colormap, (midx, midy), 4, (0, 0, 255), -1)
+    # cv2.putText(depth_colormap, "{} mm".format(depthpoint), (midx, midy - 10), 0, 1, (0, 0, 255), 2)
+    cv2.circle(color_image, (XYfarthest[0], XYfarthest[1]), 4, (0, 0, 255), -1)
+    cv2.putText(color_image, "HERE", (XYfarthest[0], XYfarthest[1] - 10), 0, 1, (0, 0, 255), 2)
+    cv2.putText(color_image, "EVASION MODE: {}".format(evasionmode[0]), (20,40), 0, 1, (0, 0, 255), 2)
+    # time.sleep(0.05)
+
+    # depth_colormap_dim = depth_colormap.shape
+    # color_colormap_dim = color_image.shape
+
+    images = np.hstack((depthHALCON_colormap, color_image))
+
+    # Show images
+    cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('RealSense', images)
+    cv2.waitKey(1)
+
+
+def main(args=None):
+    #[xresolution, yresolution, framescamera, multi, treshold] = variablesStart()
+    serialcom = seriaInitSerial()
+    externalcall = halconInitialize()
+    initialVariables = initvariables()
+    midx = int(initialVariables.xresolution/2)
+    midy = int(initialVariables.yresolution/2)
+    oldXYfarthest = [0, 0]
+    flagevasion = False
+    latestevation = 0
+    latespoint = [0, 0]
+    latesdirection = [0.0, 0.0, 0.0]
+    stabilitytimerstart = 0
+    stabilitytimerend = 0
+    x=0.0
+    y=0.0
+    z=0.0
+
+
+    oldimagehalcon =np.zeros((initialVariables.xresolution,initialVariables.yresolution), dtype=np.int8)
+    if externalcall == 0:
+        exit(0)
+    pipeline = cameraInitialize(initialVariables.xresolution, initialVariables.yresolution, initialVariables.framescamera)
+    if pipeline == 0:
+        exit(0)
+    
+    
+    try:
+        while True:
+
+            start = time.time()
+            [depth_image, color_image] = cameraData(pipeline)
+            # depthpoint = int(depth_image[midy, midx])
+            [imageReturnHalcon2, XYfarthest, deptVal, evasionmode, newdistance] = halconProcessing(depth_image, externalcall, initialVariables, oldXYfarthest, oldimagehalcon)
+            
+
+            if (evasionmode[0] == 1) and (flagevasion == False):
+                stabilitytimerstart = time.time()
+                latestevation = evasionmode
+                latespoint = XYfarthest
+                latesdirection = newdistance
+                flagevasion = True
+
+            if flagevasion:
+                evasionmode = latestevation
+                XYfarthest = latespoint
+                newdistance = latesdirection
+
+            #print("Farthest point area: ", XYfarthest)
+            print(newdistance)
+            oldXYfarthest = XYfarthest
+            oldimagehalcon = imageReturnHalcon2
+            end = time.time()
+            stabilitytimerend = time.time()
+            if (stabilitytimerend - stabilitytimerstart) > 1.0:
+                flagevasion = False
+            #print("FPS: ", round(1 / (end - start)))
+            print(type(newdistance[0]))
+            showImages(depth_image, imageReturnHalcon2, color_image, deptVal, XYfarthest, evasionmode, midx, midy)
+            
+            
+            value = [float(evasionmode[0]), newdistance[0], newdistance[1], newdistance[2]]
+            valuetoString = ' '.join([str(s) for s in value])
+            
+            serialcom.write(valuetoString.encode('utf-8'))
+            nodo.send_message(value)
+    except KeyboardInterrupt:
+        print("Closing program")
+    finally:
+        pipeline.stop()
+        if serialcom and serialcom.is_open:
+            serialcom.close()
+
+
+# MAIN CALL
+main()
